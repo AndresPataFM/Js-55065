4345dc2a4603c30254e5192272b997ec03e49d76
Andrés Pata Fraile de Manterola <apfm21@Gmail.com>
2023-08-07 20:34:42 -0300

    clase 4

---
+++ Clase 4/practica.js
@@ -0,0 +1,34 @@
+// objetos literales (15-20)
+
+// Arrays (15)
+
+// declarar
+
+// acceder
+
+// length
+
+// metodos (45)
+
+// push
+// unshift
+
+// shift
+// pop
+
+// join
+
+// === Break === (10)
+
+// indexOf
+// includes 
+
+// sort
+
+// reverse
+
+// For Of (5-10)
+
+// Micro
+
+// Pre-Entrega
