d8c1468afb3ff665fcc406c733aac77d9f58513f
Andrés Pata Fraile de Manterola <apfm21@Gmail.com>
2023-08-14 23:06:57 -0300

    final clase 5

---
+++ Clase 5/static.js
@@ -0,0 +1,12 @@
+class Gato{
+    constructor(nombre){
+        this.nombre = nombre
+    }
+    static maullar = function(){
+        alert('miau')
+    }
+}
+
+const michifus = new Gato("Michifus")
+
+Gato.maullar()
