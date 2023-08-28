c5cc95b9da77b8ef2a295c281a96487b77f83511
Andrés Pata Fraile de Manterola <apfm21@Gmail.com>
2023-08-28 20:24:35 -0300

    inicio clase 6

---
+++ Clase 6/enVivo.js
@@ -0,0 +1,25 @@
+// Funciones de orden superior
+
+// Por return
+
+// Funciones recursivas
+
+// Por parámetro
+
+// Array
+
+// forEach()
+
+// find()
+
+// filter()
+
+// some()
+
+// map()
+
+// reduce(acumulador, elemento) 
+
+// Sort
+// menor a mayor
+// mayor a menor
